fbe716175d48a7ac2cae63509f330d42d1b6e545
Matt Fenwick <mfenwick100@gmail.com>
2014-03-29 22:40:21 -0400

    start testing with mocha

---
+++ test/serialize.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var S = require('../lib/serialize'),
+    assert = require('assert');
+
+var module = describe,
+    test = it,
+    deepEqual = assert.deepEqual;
+
+
+module("serialize", function() {
+
+});
+
